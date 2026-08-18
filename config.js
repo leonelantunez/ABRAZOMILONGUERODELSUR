664910b2bbed6cb4ae0c8c60beaa560f0a14b4be
leonelantunez <leonelantunez@gmail.com>
2026-08-18 18:21:04 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const GITHUB_CONFIG = {
   username: "leonelantunez",
-  repo: "ABRAZOMILONGUERODELSUR",
+  repo: "abrazo2",
   path: "js/milongas_data.js",
-  token: "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"
+  token: "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"
 };
