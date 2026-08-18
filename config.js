5ceeffb28b5fe5038b1368be2b0006b27d213750
leonelantunez <leonelantunez@gmail.com>
2026-08-18 16:17:04 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const GITHUB_CONFIG = {
+  username: "leonelantunez",
+  repo: "ABRAZOMILONGUERODELSUR",
+  path: "js/milongas_data.js",
+  token: "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"
+};
